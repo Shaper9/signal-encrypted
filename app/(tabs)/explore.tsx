c1ec7a0e35463d20fd5923e0a7e369f7287b4e41
Marko Krstin <marko.krstin@envisionpharma.com>
2024-08-19 15:13:37 +0200

    added qr code generator

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,7 +1,32 @@
-import { StyleSheet, Image, Platform, Text } from "react-native";
+import { StyleSheet, Image, Platform, Text, View } from "react-native";
+import QRCode from "react-native-qrcode-svg";
 
 export default function TabTwoScreen() {
-  return <Text>tab two</Text>;
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.qrWrapper}>
+        <Text style={styles.qrHeader}>Your ID</Text>
+        <QRCode value="http://awesome.link.qr" size={200} />
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  qrWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  qrHeader: {
+    marginBottom: 20,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
