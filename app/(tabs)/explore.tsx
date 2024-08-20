637f81046dabfa7b1d7f4251f220fde30d855c5b
Marko Krstin <marko.krstin@envisionpharma.com>
2024-08-20 09:37:14 +0200

    added QR code scanner component

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,13 +1,28 @@
 import { StyleSheet, Image, Platform, Text, View } from "react-native";
 import QRCode from "react-native-qrcode-svg";
+import QRCodeScanner from "react-native-qrcode-scanner";
+import { RNCamera } from "react-native-camera";
 
 export default function TabTwoScreen() {
+  const onSuccess = (e: any) => {
+    console.log(e);
+  };
   return (
     <View style={styles.wrapper}>
       <View style={styles.qrWrapper}>
         <Text style={styles.qrHeader}>Your ID</Text>
         <QRCode value="http://awesome.link.qr" size={200} />
       </View>
+      {/* <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text>
+            Go to <Text>wikipedia.org/wiki/QR_code</Text> on your computer and
+            scan the QR code.
+          </Text>
+        }
+      /> */}
     </View>
   );
 }
